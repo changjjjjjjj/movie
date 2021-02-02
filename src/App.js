0e5b3c93c41d3760767f15e3bf9ebd863b6b6378
changjjjjjjjj <light9632@gmail.com>
2021-02-02 22:37:23 +0900

    Props study

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import Test from "./Test";
+import PropTypes from "prop-types"
+
+function Movie({Title, series}){
+  return (
+    <h1>My favorite movie is {Title} {series}</h1>
+  );
+}
+
+
+const movieILike = [{id:1, Title: "Irom Man", series: 1}, {id:2, Title: "Captain America", series: 2}, {id:3, Title: "Hulk", series: 2}];
+
+function renderMovie(movie){
+  return <Movie key={movie.id} Title={movie.Title} series={movie.series} />
+}
+
+Movie.propTypes = {
+  Title: PropTypes.string.isRequired,
+  series: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div className="App">
-      Hello?
-      <Test/>
+      {movieILike.map(renderMovie)};
     </div>
   );
 }
