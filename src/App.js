a652516c0815378621e51cad4366b2d141261878
changjjjjjjjj <light9632@gmail.com>
2021-02-04 21:39:45 +0900

    state base

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import PropTypes from "prop-types"
+import React from "react";
 
-function Movie({Title, series}){
-  return (
-    <h1>My favorite movie is {Title} {series}</h1>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading:false});
+    }, 6000)
+  }
+
+  render(){
+    console.log("rendering");
+    const {isLoading} = this.state;
+    return (
+      <div>{isLoading? "Loading..": "Ready"}</div>
+    );
+  }
 }
-
-
-const movieILike = [{id:1, Title: "Irom Man", series: 1}, {id:2, Title: "Captain America", series: 2}, {id:3, Title: "Hulk", series: 2}];
-
-function renderMovie(movie){
-  return <Movie key={movie.id} Title={movie.Title} series={movie.series} />
-}
-
-Movie.propTypes = {
-  Title: PropTypes.string.isRequired,
-  series: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div className="App">
-      {movieILike.map(renderMovie)};
-    </div>
-  );
-}
-
 export default App;
