5d5c514a4f33356fa26169f719bd0c046cd9b795
changjjjjjjjj <light9632@gmail.com>
2021-02-10 23:19:40 +0900

    route example

---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About(props){
+    return <span>Router test</span>;
+}
+
+export default About;
---
+++ src/routes/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+
+class Home extends React.Component{
+    state = {
+      isLoading: true
+    };
+  
+    getMovies = async() => {
+      const {data:{data:{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+      this.setState({ movies, isLoading:false });
+    }
+  
+    componentDidMount(){
+      this.getMovies();
+    }
+  
+    render(){
+      console.log("rendering");
+      const {isLoading, movies} = this.state;
+      return (
+        <section className="container">
+          {isLoading? (
+            <div className="loader_text">
+              <span>Loading..</span>
+            </div>
+          ): (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie key={movie.id} id = {movie.id} year = {movie.year} title = {movie.title} summary = {movie.summary} poster = {movie.medium_cover_image} genres ={movie.genres} />
+              ))}
+            </div>
+          )}
+        </section>
+      );
+    }
+  }
+
+  export default Home;
